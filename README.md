c52c371e1ea09d086c0fc5eb9982bcda7e326728
AH Uyekita <anderson.uyekita@gmail.com>
2022-05-23 23:58:31 -0300

    New run_analysis.R with comments.

--- README.md
+++ README.md
@@ -4,10 +4,9 @@
 
 -   Specialization: Data Science: Foundations using R Specialization
 -   Course: Getting and Cleaning Data
-    -   Instructor: Jeff Leek
+    -   Instructor: Jeffrey Leek
     -   URL: <https://www.coursera.org/learn/data-cleaning>
-    -   From: 20/05/2022
-    -   To: XX/05/2022
+    -   From: 23/05/2022
 
 ------------------------------------------------------------------------
 
@@ -15,13 +14,30 @@
 
 This course project aims to acquire data from the internet and build a
 cleaned dataset, the so-called tidy dataset. Also, we need to the
-registry the data manipulation in the CodeBook.md. The project consists
-of loading 7 txt files and bringing them together into a single dataset
-to be used in further analysis (not covered by this course). In
+registry the data manipulation in the `CodeBook.md`. The project
+consists of loading 7 txt files and bringing them together into a single
+dataset to be used in further analysis (not covered by this course). In
 addition, neither all data on this dataset will be used, so it was
-necessary to filter its measurements’ mean and standard deviation.
+necessary to filter its measurements' mean and standard deviation.
 
--   [CodeBook.md](./CodeBook.md)
+The Script is divided into 5 parts:
+
+1.  Getting Data: Steps to download the files
+2.  Loading Data: Steps to import text files into R objects
+3.  Cleaning and Manipulating Data: The Loading Data process has
+    imported several single files, so to do further analysis, it is
+    necessary to merge those files into a single dataset. In addition,
+    as stated by the Project Instruction, the final dataset only need to
+    have the means and standard deviations.
+4.  Backup Data: The second independent dataset could be accessed by the
+    variable `df_tidy`.
+5.  Calculating the mean: This is the data manipulation to calculate the
+    mean of a grouped variable (Subject and Activity).
+6.  Exporting Data: Final step is to export the dataset with grouped
+    mean by Subject and Activity.
+
+-   For any further info about the `run_analysis.r`, please you will
+    find it in [CodeBook.md](./CodeBook.md)
 
 ------------------------------------------------------------------------
 
@@ -32,7 +48,7 @@ necessary to filter its measurements’ mean and standard deviation.
     this `source("run_analysis.R")`;
 3.  The script will be run automatically;
 4.  The tidy data could be visualized using the command `view()`. Like
-    this `view(tidy_database)`. Also, the tidy data set will be saved at
+    this `view(df_tidy_mean)`. Also, the tidy data set will be saved at
     the root (Documents).
 
 ``` r
@@ -40,7 +56,7 @@ necessary to filter its measurements’ mean and standard deviation.
 source(file = "run_analysis.r")
 
 # Viewing the tidy dataset
-view(tidy_database)
+view(df_tidy_mean)
 ```
 
 ## 2. Review criteria
@@ -55,7 +71,7 @@ view(tidy_database)
 5.  The work submitted for this project is the work of the student who
     submitted it.
 
-## 3.Project Instructions
+## 3. Project Instructions
 
 The purpose of this project is to demonstrate your ability to collect,
 work with, and clean a data set. The goal is to prepare tidy data that
@@ -74,12 +90,13 @@ You should also include a README.md in the repo with your scripts. This
 repo explains how all of the scripts work and how they are connected.
 
 One of the most exciting areas in all of data science right now is
-wearable computing - see for example this article . Companies like
-Fitbit, Nike, and Jawbone Up are racing to develop the most advanced
-algorithms to attract new users. The data linked to from the course
-website represent data collected from the accelerometers from the
-Samsung Galaxy S smartphone. A full description is available at the site
-where the data was obtained:
+wearable computing - see for example ~~[this
+article](http://www.insideactivitytracking.com/data-science-activity-tracking-and-the-battle-for-the-worlds-top-sports-brand/)~~
+. Companies like Fitbit, Nike, and Jawbone Up are racing to develop the
+most advanced algorithms to attract new users. The data linked to from
+the course website represent data collected from the accelerometers from
+the Samsung Galaxy S smartphone. A full description is available at the
+site where the data was obtained:
 
 <http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones>
 
--- run_analysis.R
+++ run_analysis.R
@@ -12,95 +12,132 @@
 ################################################################################
 
 # Loading packages
-library(plyr)
+library(tidyverse)
 library(httr)
+library(magrittr)
 
-# Creating a folder to store the data downloaded from the internet.
+################################ 1. Getting Data ###############################
+
+# 1.1. Creating a folder to store the data downloaded from the internet.
 # Checking if the subfolder already exists.
-if (!dir.exists("data")) {
+if (!base::dir.exists("data")) {
     
     # Creating a subfolder to store the data.
-    dir.create(path = "./data")
+    base::dir.create(path = "./data")
 }
 
-################################ 0. Getting Data #####################################
-
-# Downloading Data from URL
-if(!file.exists("./data/DataSet.zip")) {
+# 1.2. Downloading Data from URL
+if(!base::file.exists("./data/DataSet.zip")) {
     utils::download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
                          "./data/DataSet.zip")
 }
 
-# Unzipping the DataSet
-if(!dir.exists("./data/UCI HAR Dataset")) {
+# 1.3. Unzipping the DataSet
+if(!base::dir.exists("./data/UCI HAR Dataset")) {
     utils::unzip(zipfile = "./data/DataSet.zip",
                  exdir = "./data",
                  list = FALSE,
                  overwrite = TRUE)
 }
 
-############################ 1. Loading & Merging Data #########################
+###################### 2. Loading Training & Testing Data ######################
 
-# 1.1 Importing Train X (Attention here! X_train uses X capital instead of x)
-x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+# 2.1 Importing Train X (Attention here! X_train uses X capital instead of x)
+x_train <- utils::read.table(file = "./data/UCI HAR Dataset/train/X_train.txt",
+                             header = FALSE)
 
-# 1.2. Importing Train Y
-y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+# 2.2. Importing Train Y
+y_train <- utils::read.table("./data/UCI HAR Dataset/train/y_train.txt",
+                             header = FALSE)
 
-# 1.3. Importing Train Subject
-subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+# 2.3. Importing Train Subject
+subject_train <- utils::read.table("./data/UCI HAR Dataset/train/subject_train.txt",
+                                   header = FALSE)
 
-# 1.4 Importing Test X (Attention here! X_train uses X capital instead of x)
-x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+# 2.4 Importing Test X (Attention here! X_train uses X capital instead of x)
+x_test <- utils::read.table("./data/UCI HAR Dataset/test/X_test.txt",
+                            header = FALSE)
 
-# 1.5. Importing Test Y
-y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt") 
+# 2.5. Importing Test Y
+y_test <- utils::read.table("./data/UCI HAR Dataset/test/y_test.txt",
+                            header = FALSE) 
 
-# 1.6. Importing Test Subject
-subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+# 2.6. Importing Test Subject
+subject_test <- utils::read.table("./data/UCI HAR Dataset/test/subject_test.txt",
+                                  header = FALSE)
 
-# 1.7. Importing Features Names
-features <- read.table(file = "./data/UCI HAR Dataset/features.txt",
-                       sep = "",
-                       stringsAsFactors = FALSE)
+# 2.7. Importing Features Names
+features <- utils::read.table(file = "./data/UCI HAR Dataset/features.txt",
+                              sep = "",
+                              stringsAsFactors = FALSE,
+                              header = FALSE)
 
-# 1.8. Merging Test and Train
-database <- rbind(x_train, x_test)
-col_names_features <- c(features$V2)
-names(database) <- c(col_names_features)
+# 2.8. Importing Activities Names
+activity_labels <- utils::read.table("./data/UCI HAR Dataset/activity_labels.txt",
+                              header = FALSE)
 
-################################ 2. Subseting Data #####################################
+###################### 3. Cleaning & Manipulation Data #########################
 
-# 2.1. Vector to select column with only mean and standard deviation
-selection_mean_std <- grep("mean\\(\\)|std\\(\\)", features$V2)
+### Manipulation
 
-# 2.2. Applying the selection vector to the database
-database_mean_std <- database[,selection_mean_std]
+# 3.1. Merging Test and Train
+# Merging the X training and X testings datasets to create one data set.
+x <- as_tibble(base::rbind(x_train, x_test))
 
-# 2.3. Adding Column Subject and Activity
-database_mean_std <- cbind(rbind(subject_train, subject_test),rbind(y_train, y_test),database_mean_std)
-names(database_mean_std)[1] <- c("subject")
-names(database_mean_std)[2] <- c("activity")
+# 3.2 Merging the Y training and testing observations.
+y <- rbind(y_train, y_test)
 
-################# 3. Importing descriptive activity names #######################
+# 3.3. Merging the subjects training and testing files.
+subjects <- rbind(subject_train, subject_test)
 
-# 3.1. Importing Activities Names
-activity <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+# 3.4. Naming the columns with the features$V2 info
+base::colnames(x) <- features$V2
 
-################# 4. Changing ID to descriptive activity names #######################
+### Cleaning
 
-# 4.1. Changing ID for Descriptive Name
-database_mean_std$activity <- factor(database_mean_std$activity, levels=activity$V1, labels=activity$V2)
+# 3.5. Selecting only columns with mean() or std() values from x_train and x_test.
+x_mean_std <- x %>%
+    select(contains(c("mean()","std()")))
 
-# 4.2. Saving First Data
-write.table(database, "database_1st.txt")
 
-#################################### 5. Tidy data set  ###############################
+# 3.6. Adding Column Subject and Activity
+#
+# The df_tidy is the final dataset with all data cleaning and manipulation.
+#
+df_tidy <- x_mean_std %>%
+    
+    # Adding new columns.
+    mutate("subject" = subjects[["V1"]],   # ATTENTION!!
+           "activity" = y[["V1"]]) %>%     # It is necessary to add a vector as new column.
+                                           # This is the reason to use double [["V1"]]
+    # Reordering the columns sequence.
+    select(subject, activity, everything()) %>%
+    
+    # Converting the activity column into FACTORS
+    mutate(activity = factor(x = activity,                 # Converting levels 1 to 6 into
+                             levels = activity_labels$V1,  # descriptive variables as given
+                             labels = activity_labels$V2)) # in activity_labels.txt 
 
-# 5.1. With the average of each variable for each activity and each subject
-tidy_database <- ddply(database_mean_std, .(subject, activity), .fun=function(x){ colMeans(x[,-c(1:2)]) })
+########################## 4. Backup the Tidy Dataset ##########################
 
-colnames(tidy_database)[-c(1:2)] <- paste(colnames(tidy_database)[-c(1:2)], "_mean", sep="")
+# 4.1. Saving First Data
+write.table(df_tidy, "df_tidy_before_step5.txt")
 
-# 5.2. Saving Tidy Data
-write.table(tidy_database, "tidy_database.txt", row.name=FALSE)
+######################## 5. Calculating the mean  ##############################
+
+# 5.1. Calculating the mean of observations based on groups of subject and activity.
+df_tidy_mean <- df_tidy %>%
+    
+    # Grouping the dataset in subject and activity.
+    group_by(subject, activity) %>%
+    
+    # Calculating the mean for all columns.
+    summarise_all(mean, na.rm = TRUE) %>%
+    
+    # Renaming the columnus to have a prefix of "mean".
+    rename_at(vars(-subject,-activity),~paste0(., "_mean"))
+
+######################## 6. Exporting Data  ##############################
+
+# 6.1. Exporting the Dataset after Step 5.
+write.table(df_tidy_mean, "df_tidy_mean.txt")
